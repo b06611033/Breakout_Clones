731b68d6f28d76bec3b24305882769b91ca4c743
b06611033 <52776608+b06611033@users.noreply.github.com>
2022-06-20 00:58:13 -0700

    Update Bricks.java

--- BreakoutClones/Bricks.java
+++ BreakoutClones/Bricks.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.BasicStroke;
 
-
     public class Bricks {
         public boolean[][] bricksArray;
         public int brickWidth;
@@ -15,13 +14,14 @@ import java.awt.BasicStroke;
             brickHeight = 240/rows;
         }
     
-        public void doDrawing(Graphics2D g) {
+        public void doDrawing(Graphics2D g, int mode) {
             for (int i = 0; i < bricksArray.length; i++) {
                 for (int j = 0; j < bricksArray[0].length; j++) {
                     if (!bricksArray[i][j]) {
-                        g.setColor(new Color(192,192,192));
+                        if (mode == 1) g.setColor(new Color(50,165,50));
+                        else if (mode == 2)  g.setColor(new Color(100,149,237));
+                        else g.setColor(new Color(75,0,130));
                         g.fillRect(100 + brickWidth*j, 50 + brickHeight*i, brickWidth,brickHeight);
-                        
                         g.setStroke(new BasicStroke(3));
                         g.setColor(new Color(240,255,240));
                         g.drawRect(100 + brickWidth*j, 50
