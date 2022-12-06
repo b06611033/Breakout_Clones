e172d0c871ff1c7a49ed6944d1d931b95022f457
b06611033 <52776608+b06611033@users.noreply.github.com>
2022-12-05 22:35:29 -0600

    Update Board.java

--- BreakoutClones/Board.java
+++ BreakoutClones/Board.java
@@ -1,4 +1,5 @@
 package BreakoutClones;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +16,10 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     private int score = 0;
     private int highScore = 0;
     private int brickNum = 30;
+    private int brickWidth = 600 / 10;
+    private int brickHeight = 240 / 8;
+    private int row = 5;
+    private int col = 6;
     private Timer timer;
     private int delay = 8;
     private int paddleX = 310;
@@ -54,99 +59,100 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     public void paint(Graphics g) {
         if (newGame) {
             if (hardMode) {
-                //background
+                // background
                 g.drawImage(img3, 1, 1, 792, 792, 1, 1, 1100, 1100, this);
-                //border
-                g.setColor(new Color(51,0,102));
+                // border
+                g.setColor(new Color(51, 0, 102));
                 g.fillRect(0, 0, 792, 5);
                 g.fillRect(782, 0, 5, 792);
                 g.fillRect(0, 0, 5, 792);
-                //paddle
-                g.setColor(new Color(25,25,112));
+                // paddle
+                g.setColor(new Color(25, 25, 112));
                 g.fillRect(paddleX, 753, paddleLength, 10);
-                //ball
-                g.setColor(new Color(255,255,255));
+                // ball
+                g.setColor(new Color(255, 255, 255));
                 g.fillOval(ballPosX, ballPosY, 20, 20);
-                //bricks
-                bricks.doDrawing((Graphics2D)g,3);
-            } 
-            else if (mediumMode) {
+                // bricks
+                bricks.doDrawing((Graphics2D) g, 3);
+            } else if (mediumMode) {
                 g.drawImage(img2, 1, 1, 792, 792, 600, 1, 1700, 1100, this);
-                //border
-                g.setColor(new Color(0,0,102));
+                // border
+                g.setColor(new Color(0, 0, 102));
                 g.fillRect(0, 0, 792, 5);
                 g.fillRect(782, 0, 5, 792);
                 g.fillRect(0, 0, 5, 792);
-                //paddle
+                // paddle
                 g.setColor(new Color(255, 127, 80));
                 g.fillRect(paddleX, 753, paddleLength, 10);
-                //ball
-                g.setColor(new Color(127,255,212));
+                // ball
+                g.setColor(new Color(127, 255, 212));
                 g.fillOval(ballPosX, ballPosY, 20, 20);
-                //bricks
-                bricks.doDrawing((Graphics2D)g,2);
-            } 
-            else {
+                // bricks
+                bricks.doDrawing((Graphics2D) g, 2);
+            } else {
                 g.drawImage(img1, 1, 1, 792, 792, 500, 1, 1600, 1500, this);
-                //border
-                g.setColor(new Color(34,139,34));
+                // border
+                g.setColor(new Color(34, 139, 34));
                 g.fillRect(0, 0, 792, 5);
                 g.fillRect(782, 0, 5, 792);
                 g.fillRect(0, 0, 5, 792);
-                //paddle
-                g.setColor(new Color(165,42,42));
+                // paddle
+                g.setColor(new Color(165, 42, 42));
                 g.fillRect(paddleX, 753, paddleLength, 10);
-                //ball
-                g.setColor(new Color(0,255,0));
+                // ball
+                g.setColor(new Color(0, 255, 0));
                 g.fillOval(ballPosX, ballPosY, 20, 20);
-                //bricks
-                bricks.doDrawing((Graphics2D)g,1);
-            } 
+                // bricks
+                bricks.doDrawing((Graphics2D) g, 1);
+            }
         }
-        //score
-        if (mediumMode) g.setColor(new Color(0,0,0));
-        else g.setColor(new Color(255,255,255));
+        // score
+        if (mediumMode)
+            g.setColor(new Color(0, 0, 0));
+        else
+            g.setColor(new Color(255, 255, 255));
         g.setFont(new Font("serif", Font.BOLD, 25));
         g.drawString("Score: " + score, 620, 450);
         g.drawString("High Score: " + highScore, 620, 25);
-        //game states
+        // game states
         if (firstGame) {
-            g.setColor(new Color(0,0,0));
-            g.drawString("Press 1 to start in easy mode!" , 150, 510);
-            g.drawString("Press 2 to start in medium mode!" , 150, 540);
-            g.drawString("Press 3 to start in hard mode!" , 150, 570);
+            g.setColor(new Color(0, 0, 0));
+            g.drawString("Press 1 to start in easy mode!", 150, 510);
+            g.drawString("Press 2 to start in medium mode!", 150, 540);
+            g.drawString("Press 3 to start in hard mode!", 150, 570);
         }
         if (showInstructions) {
-            if (mediumMode) g.setColor(new Color(0,0,0));
-            else g.setColor(new Color(255,255,255));
+            if (mediumMode)
+                g.setColor(new Color(0, 0, 0));
+            else
+                g.setColor(new Color(255, 255, 255));
             g.setFont(new Font("serif", Font.BOLD, 25));
-            g.drawString("Press left or right arrow to start!" , 220, 25);
-        }
-        else if (score == brickNum) {
+            g.drawString("Press left or right arrow to start!", 220, 25);
+        } else if (score == brickNum) {
             play = false;
             newGame = false;
-            g.setColor(new Color(255,215,0));
+            g.setColor(new Color(255, 215, 0));
             g.setFont(new Font("serif", Font.BOLD, 72));
-            g.drawString("You won!" , 150, 450);
-            g.setColor(new Color(255,255,255));
+            g.drawString("You won!", 150, 450);
+            g.setColor(new Color(255, 255, 255));
             g.setFont(new Font("serif", Font.BOLD, 25));
-            g.drawString("Press 1 to restart in easy mode!" , 150, 510);
-            g.drawString("Press 2 to restart in medium mode!" , 150, 540);
-            g.drawString("Press 3 to restart in hard mode!" , 150, 570);
-        }
-        else if (ballPosY > 765) {
+            g.drawString("Press 1 to restart in easy mode!", 150, 510);
+            g.drawString("Press 2 to restart in medium mode!", 150, 540);
+            g.drawString("Press 3 to restart in hard mode!", 150, 570);
+        } else if (ballPosY > 765) {
             play = false;
             newGame = false;
-            g.setColor(new Color(255,0,0));
+            g.setColor(new Color(255, 0, 0));
             g.setFont(new Font("serif", Font.BOLD, 72));
-            g.drawString("Game Over!" , 150, 400);
+            g.drawString("Game Over!", 150, 400);
             g.setFont(new Font("serif", Font.BOLD, 36));
-            if (score == highScore)  g.drawString("New High Score!", 150, 450);
-            g.setColor(new Color(255,255,255));
+            if (score == highScore)
+                g.drawString("New High Score!", 150, 450);
+            g.setColor(new Color(255, 255, 255));
             g.setFont(new Font("serif", Font.BOLD, 25));
-            g.drawString("Press 1 to restart in easy mode!" , 150, 510);
-            g.drawString("Press 2 to restart in medium mode!" , 150, 540);
-            g.drawString("Press 3 to restart in hard mode!" , 150, 570);
+            g.drawString("Press 1 to restart in easy mode!", 150, 510);
+            g.drawString("Press 2 to restart in medium mode!", 150, 540);
+            g.drawString("Press 3 to restart in hard mode!", 150, 570);
             g.setFont(new Font("serif", Font.BOLD, 50));
         }
     }
@@ -155,31 +161,40 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     public void actionPerformed(ActionEvent e) {
         timer.start();
         if (play) {
-           ballPosX += ballDirX;
-           ballPosY += ballDirY; 
-           if (ballPosY > 730 && ballPosY < 740 && ballPosX + 15 > paddleX && ballPosX + 5 < paddleX + paddleLength) ballDirY = -ballDirY;  // collision with paddle
-           if (ballPosX > 765 || ballPosX < 0) ballDirX = -ballDirX; // collision with boundaries
-           if (ballPosY < 10) ballDirY = -ballDirY; // collision with boundaries
-           // collision with bricks
-           for (int i = 0; i < bricks.bricksArray.length; i++) {
-               boolean collided = false;
-               for (int j = 0; j < bricks.bricksArray[0].length; j++) {
-                   if (!bricks.bricksArray[i][j]) {
+            ballPosX += ballDirX;
+            ballPosY += ballDirY;
+            if (ballPosY > 730 && ballPosY < 740 && ballPosX + 15 > paddleX && ballPosX + 5 < paddleX + paddleLength)
+                ballDirY = -ballDirY; // collision with paddle
+            if (ballPosX > 765 || ballPosX < 0)
+                ballDirX = -ballDirX; // collision with boundaries
+            if (ballPosY < 10)
+                ballDirY = -ballDirY; // collision with boundaries
+            // collision with bricks
+            for (int i = 0; i < row; i++) {
+                boolean collided = false;
+                for (int j = 0; j < col; j++) {
+                    if (bricks.brickExists(i, j)) {
                         Rectangle ballRect = new Rectangle(ballPosX, ballPosY, 20, 20);
-                        Rectangle brickRect = new Rectangle(100 + bricks.brickWidth*j, 50 + bricks.brickHeight*i, bricks.brickWidth, bricks.brickHeight);
+                        Rectangle brickRect = new Rectangle(100 + brickWidth * j, 50 + brickHeight * i,
+                                brickWidth, brickHeight);
                         if (ballRect.intersects(brickRect)) {
                             score++;
-                            if (score > highScore) highScore = score;
-                            if (ballPosX + 19 <= 100 + bricks.brickWidth*j || ballPosX + 1 >= 100 + bricks.brickWidth*(j + 1)) ballDirX = -ballDirX;
-                            else ballDirY = -ballDirY;
+                            if (score > highScore)
+                                highScore = score;
+                            if (ballPosX + 19 <= 100 + brickWidth * j
+                                    || ballPosX + 1 >= 100 + brickWidth * (j + 1))
+                                ballDirX = -ballDirX;
+                            else
+                                ballDirY = -ballDirY;
                             bricks.brickDestroyed(i, j);
                             collided = true;
                             break;
                         }
-                   }
-               }
-               if (collided) break;
-           }
+                    }
+                }
+                if (collided)
+                    break;
+            }
         }
         repaint();
     }
@@ -191,22 +206,28 @@ public class Board extends JPanel implements KeyListener, ActionListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT && newGame) {
-            if (paddleX > 680) paddleX = 700;
-            else moveRight();
-        }
-        else if (e.getKeyCode() == KeyEvent.VK_LEFT && newGame) {
-            if (paddleX < 25) paddleX = 5;
-            else moveLeft();
-        }
-        else if (e.getKeyCode() == KeyEvent.VK_1 && !newGame) {
-            if(!play) {
+            if (paddleX > 680)
+                paddleX = 700;
+            else
+                moveRight();
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && newGame) {
+            if (paddleX < 25)
+                paddleX = 5;
+            else
+                moveLeft();
+        } else if (e.getKeyCode() == KeyEvent.VK_1 && !newGame) {
+            if (!play) {
                 score = 0;
                 paddleX = 310;
                 ballPosX = 60 + r.nextInt(500);
                 ballPosY = 350 + r.nextInt(200);
                 ballDirX = -3 - r.nextInt(1);
                 ballDirY = -3 - r.nextInt(1);
-                bricks = new Bricks(5,6);
+                row = 5;
+                col = 6;
+                bricks = new Bricks(row, col);
+                brickWidth = 600 / 6;
+                brickHeight = 240 / 5;
                 brickNum = 30;
                 firstGame = false;
                 showInstructions = true;
@@ -216,16 +237,19 @@ public class Board extends JPanel implements KeyListener, ActionListener {
                 hardMode = false;
 
             }
-        }
-        else if (e.getKeyCode() == KeyEvent.VK_2 && !newGame) {
-            if(!play) {
+        } else if (e.getKeyCode() == KeyEvent.VK_2 && !newGame) {
+            if (!play) {
                 score = 0;
                 paddleX = 310;
                 ballPosX = 60 + r.nextInt(500);
                 ballPosY = 350 + r.nextInt(200);
                 ballDirX = -4 - r.nextInt(2);
                 ballDirY = -4 - r.nextInt(2);
-                bricks = new Bricks(6,8);
+                row = 6;
+                col = 8;
+                bricks = new Bricks(row, col);
+                brickWidth = 600 / 8;
+                brickHeight = 240 / 6;
                 brickNum = 48;
                 firstGame = false;
                 showInstructions = true;
@@ -234,16 +258,19 @@ public class Board extends JPanel implements KeyListener, ActionListener {
                 mediumMode = true;
                 hardMode = false;
             }
-        }
-        else if (e.getKeyCode() == KeyEvent.VK_3 && !newGame) {
-            if(!play) {
+        } else if (e.getKeyCode() == KeyEvent.VK_3 && !newGame) {
+            if (!play) {
                 score = 0;
                 paddleX = 310;
                 ballPosX = 60 + r.nextInt(500);
                 ballPosY = 350 + r.nextInt(200);
                 ballDirX = -6 - r.nextInt(1);
                 ballDirY = -6 - r.nextInt(1);
-                bricks = new Bricks(8,10);
+                row = 8;
+                col = 10;
+                bricks = new Bricks(row, col);
+                brickWidth = 600 / 10;
+                brickHeight = 240 / 8;
                 brickNum = 80;
                 firstGame = false;
                 showInstructions = true;
@@ -255,12 +282,13 @@ public class Board extends JPanel implements KeyListener, ActionListener {
         }
 
     }
-    
+
     public void moveRight() {
         play = true;
         showInstructions = false;
         paddleX += 30;
     }
+
     private void moveLeft() {
         play = true;
         showInstructions = false;
